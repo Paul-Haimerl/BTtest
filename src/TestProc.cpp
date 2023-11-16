f1f5f645f2ffadbcf1e267061b179efcf1e63250
Paul-Haimerl <paul.haimerl@gmx.de>
2023-11-16 03:54:49 +0100

    extensions

--- src/TestProc.cpp
+++ src/TestProc.cpp
@@ -1,19 +1,19 @@
+
 #include <RcppArmadillo.h>
-#include <cmath>
-using namespace arma;
-using namespace Rcpp;
 // [[Rcpp::depends(RcppArmadillo)]]
+#include <cmath>
+using namespace Rcpp;
 
 
 //' Obtain a standardization factor for the eigenvalues according to Eq. 18
-//'
-//' @param nu ordered (descending) vector of eigenvalues
-//' @param k censoring threshold. Largest k-1 eigenvalues are omitted
-//'
-//' @return Scaling factor
+ //'
+ //' @param nu ordered (descending) vector of eigenvalues
+ //' @param k censoring threshold. Largest k-1 eigenvalues are omitted
+ //'
+ //' @return Scaling factor
 
- double EigenNorm(colvec nu, int k){
-     colvec nu_red;
+ double EigenNorm(arma::colvec nu, int k){
+     arma::colvec nu_red;
      if (k != 0){
          nu_red = nu.tail(nu.size() - k + 1);
      } else {
@@ -24,62 +24,62 @@ using namespace Rcpp;
  }
 
 
-//' Obtain a matrix of properly scaled eigenvalues
-//'
-//' @param X (T x N) matrix of observations
-//' @param T number of time periods
-//' @param N number of cross-sectional units
-//' @param delta scaling factor for N
-//'
-//' @return (N x r) matrix of scaled eigenvalues
+ //' Obtain a matrix of properly scaled eigenvalues
+ //'
+ //' @param X (T x N) matrix of observations
+ //' @param T number of time periods
+ //' @param N number of cross-sectional units
+ //' @param delta scaling factor for N
+ //'
+ //' @return (N x r) matrix of scaled eigenvalues
 
- mat StandEigVals (mat X, int T, int N, double delta){
-     mat XtX = trans(X) * X;
-     mat DeltaX = diff(X, 1);
+ arma::mat StandEigVals (arma::mat X, int T, int N, double delta){
+     arma::mat XtX = trans(X) * X;
+     arma::mat DeltaX = diff(X, 1);
 
-     mat Sigma_1 = XtX / pow(T, 3);
-     mat Sigma_2 = XtX / pow(T, 2);
-     mat Sigma_3 = (trans(DeltaX) * DeltaX) / T;
+     arma::mat Sigma_1 = XtX / pow(T, 3);
+     arma::mat Sigma_2 = XtX / pow(T, 2);
+     arma::mat Sigma_3 = (trans(DeltaX) * DeltaX) / T;
 
      // Eq. 9
-     colvec nu_1 = sort(eig_sym(Sigma_1), "descend");
+     arma::colvec nu_1 = sort(arma::eig_sym(Sigma_1), "descend");
      // Eq. 10
-     colvec nu_2 = sort(eig_sym(Sigma_2), "descend");
+     arma::colvec nu_2 = sort(arma::eig_sym(Sigma_2), "descend");
      // Eq. 17
-     colvec nu_3 = sort(eig_sym(Sigma_3), "descend");
+     arma::colvec nu_3 = sort(arma::eig_sym(Sigma_3), "descend");
 
      // Apply scaling
      // Eq. 18
      double nu_3_bar = EigenNorm(nu_3, 0);
      // Eq. 20
-     colvec phi_1 = exp(pow(N, -delta) * nu_1 / nu_3_bar);
+     arma::colvec phi_1 = exp(pow(N, -delta) * nu_1 / nu_3_bar);
      // Eq. 25
-     colvec phi_2 = exp(pow(N, -delta) * log(log(T)) * nu_2 / nu_3_bar);
-     colvec phi_3 = exp(pow(N, -delta) * nu_3 / nu_3_bar);
+     arma::colvec phi_2 = exp(pow(N, -delta) * log(log(T)) * nu_2 / nu_3_bar);
+     arma::colvec phi_3 = exp(pow(N, -delta) * nu_3 / nu_3_bar);
 
-     mat Phi = join_rows(phi_1, phi_2, phi_3);
+     arma::mat Phi = join_rows(phi_1, phi_2, phi_3);
      return Phi;
  }
 
 
-//' @description Runs the randomized testing routine
-//'
-//' @param phi vector of scaled eigenvalues
-//' @param indx for the factor group in question
-//' @param p pth largest eigenvalue to consider
-//' @param R Number of simulated random variables
-//' @param u threshold to define a Bernoulli sequence
-//'
-//' @return p-value of the test
+ //' Runs the randomized testing routine
+ //'
+ //' @param phi vector of scaled eigenvalues
+ //' @param indx for the factor group in question
+ //' @param p pth largest eigenvalue to consider
+ //' @param R Number of simulated random variables
+ //' @param u threshold to define a Bernoulli sequence
+ //'
+ //' @return p-value of the test
 
- float RandomTest (mat Phi, int indx, int p, int R, float u){
+ float RandomTest (arma::mat Phi, int indx, int p, int R, float u){
      float phi = Phi.col(indx - 1)(p - 1);
      // Step A1.1
-     vec xi(R, fill::randn);
+     arma::vec xi(R, arma::fill::randn);
      // Step A1.2
-     uvec zeta = conv_to<uvec>::from(xi * phi <= u);
+     arma::uvec zeta = arma::conv_to<arma::uvec>::from(xi * phi <= u);
      // Step A1.3
-     float zeta_sum = accu(zeta);
+     float zeta_sum = arma::accu(zeta);
      float varrho = 1.0 / sqrt(R) * (zeta_sum - R * .5);
      // Step A1.4
      float tstat = pow(varrho, 2);
@@ -88,17 +88,17 @@ using namespace Rcpp;
  }
 
 
-//' @description Runs the iterative randomized testing routine
-//'
-//' @param phi vector of scaled eigenvalues
-//' @param rmax maximum number of ordered eigenvalues to consider
-//' @param sig significance level
-//' @param R Number of simulated random variables per iteration
-//' @param u threshold to define a Bernoulli sequence
-//'
-//' @return estimated number of factors
+ //' Runs the iterative randomized testing routine
+ //'
+ //' @param phi vector of scaled eigenvalues
+ //' @param rmax maximum number of ordered eigenvalues to consider
+ //' @param sig significance level
+ //' @param R Number of simulated random variables per iteration
+ //' @param u threshold to define a Bernoulli sequence
+ //'
+ //' @return estimated number of factors
 
- int RandomTestWrapper(mat Phi, int indx, int rmax, float sig, int R, float u){
+ int RandomTestWrapper(arma::mat Phi, int indx, int rmax, float sig, int R, float u){
      int r_hat = 0;
      // Iteratively test the different eigenvalues
      for (int r = 1; r <= rmax; r++) {
@@ -113,71 +113,70 @@ using namespace Rcpp;
  }
 
 
-//' @description Runs the Barigozzi Trapani test for the number of factors
-//'
-//' @param X (T x N) matrix of observations
-//' @param rmax maximum number of factors to consider
-//' @param alpha significance level
-//'
-//' @return vector with the estimated number of factors
-// [[Rcpp::export]]
+ //' Runs the Barigozzi Trapani test for the number of factors
+ //'
+ //' @param X (T x N) matrix of observations
+ //' @param rmax maximum number of factors to consider
+ //' @param alpha significance level
+ //'
+ //' @return vector with the estimated number of factors
+ //'
+ // [[Rcpp::export]]
 
-NumericVector nFactors (mat X, int rmax, double alpha){
+ NumericVector nFactors (arma::mat X, int rmax, double alpha){
 
-//------------------------------//
-// Preliminaries                //
-//------------------------------//
+     //------------------------------//
+     // Preliminaries                //
+     //------------------------------//
 
-    int T = X.n_rows;
-    int N = X.n_cols;
-    double beta = log(N) / log(T);
-    double delta;
-    if (beta < .5){
-        delta = 1e-5;
-    } else {
-        delta = 1e-5 + 1 - .5 / beta;
-    }
-    double sig = alpha / fmin(N, T);
-    double u = sqrt(2);
+     int T = X.n_rows;
+     int N = X.n_cols;
+     double beta = log(N) / log(T);
+     double delta;
+     if (beta < .5){
+         delta = 1e-5;
+     } else {
+         delta = 1e-5 + 1 - .5 / beta;
+     }
+     double sig = alpha / fmin(N, T);
+     double u = sqrt(2);
 
-    //------------------------------//
-    // Obtain the eigenvalues       //
-    //------------------------------//
+     //------------------------------//
+     // Obtain the eigenvalues       //
+     //------------------------------//
 
-    mat Phi = StandEigVals(X, T, N, delta);
+     arma::mat Phi = StandEigVals(X, T, N, delta);
 
-    //------------------------------//
-    // Test for r_1                 //
-    //------------------------------//
+     //------------------------------//
+     // Test for r_1                 //
+     //------------------------------//
 
-    float pvalue_1 = RandomTest(Phi, 1, 1, N, u);
-    int r_1_hat;
-    if(pvalue_1 > sig){
-        r_1_hat = 1;
-    } else {
-        r_1_hat = 0;
-    }
+     float pvalue_1 = RandomTest(Phi, 1, 1, N, u);
+     int r_1_hat;
+     if(pvalue_1 > sig){
+         r_1_hat = 1;
+     } else {
+         r_1_hat = 0;
+     }
 
-    //------------------------------//
-    // Test for r_star              //
-    //------------------------------//
+     //------------------------------//
+     // Test for r_star              //
+     //------------------------------//
 
-    int r_star_hat = RandomTestWrapper(Phi, 2, rmax, sig, N, u);
-    int r_2_hat = r_star_hat - r_1_hat;
+     int r_star_hat = RandomTestWrapper(Phi, 2, rmax, sig, N, u);
+     int r_2_hat = r_star_hat - r_1_hat;
 
-    //------------------------------//
-    // Test for r                   //
-    //------------------------------//
+     //------------------------------//
+     // Test for r                   //
+     //------------------------------//
 
-    int r_hat = RandomTestWrapper(Phi, 3, rmax, sig, N, u);
-    int r_3_hat = r_hat - r_star_hat;
+     int r_hat = RandomTestWrapper(Phi, 3, rmax, sig, N, u);
+     int r_3_hat = r_hat - r_star_hat;
 
-    NumericVector output = NumericVector::create(
-       Named("r_1_hat") = r_1_hat,
-       Named("r_2_hat") = r_2_hat,
-       Named("r_3_hat") = r_3_hat
-    );
-    return output;
-}
-
-
+     NumericVector output = NumericVector::create(
+         Named("r_1_hat") = r_1_hat,
+         Named("r_2_hat") = r_2_hat,
+         Named("r_3_hat") = r_3_hat
+     );
+     return output;
+ }
